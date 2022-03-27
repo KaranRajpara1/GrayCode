813785dcf43b1916fd5696573db773cfdbacc851
Karan Rajpara <karan.rajpara107598@marwadiuniversity.ac.in>
2022-03-28 00:43:56 +0530

    Intial commit

---
+++ src/Logic.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author KaranRajpara
+ */
+public class Logic {
+      static int binary[]=new int[8];
+       
+        
+        public void gray(){
+            
+        }
+}
